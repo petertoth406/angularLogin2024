e0efd2a8bae71d2c8ef606d8b005762add99acda
petertoth406 <petertoth406@gmail.com>
2024-03-21 18:41:07 +0100

    FormControl Ujra validálás

--- src/app/new-product/new-product.component.ts
+++ src/app/new-product/new-product.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class NewProductComponent implements OnInit {
 
   form: FormGroup;
+  invalidNameClass: string =  "alma";
+
+  invalidControls:any [] = [];
 
   constructor(
     private fb: FormBuilder
@@ -38,6 +41,86 @@ export class NewProductComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    /*this.form.valueChanges.subscribe(
+      
+      value=>{
+        console.log(value)
+      },
+      err=>{
+        console.error(err);
+      },
+      ()=>{
+        console.log("Form changed");
+      }
+    )*/
+
+      this.form.valueChanges.subscribe(value=>{
+        if(this.form.get("name")?.invalid){
+          this.invalidNameClass = "error";
+        }else if(this.form.get("name")?.valid){
+          this.invalidNameClass = "";
+        }
+
+        this.invalidControls = [];
+
+        for(let control in this.form.controls){
+          if(this.form.get(control)?.invalid){
+            this.invalidControls.push(control);
+          }
+        }
+
+        console.log(this.invalidControls);
+      
+      })
+
+    this.form.get('name')?.valueChanges.subscribe(
+      value=>{
+
+        let text = "A termék neve " + value.length + " karakter hosszú";
+
+        this.form.get("description")?.patchValue(text);
+      }
+        
+    )
+
+    /*
+      Elektronikai cikk: 5000 - 500 000
+      Ruha: n - 50 000
+      Étel: 100 - 10 000
+    */
+
+      this.form.get("category")?.valueChanges.subscribe(
+        value => {
+          console.log(value)
+          switch(value){
+            case "electronics": 
+              this.form.get("price")?.clearValidators();
+
+              this.form.get("price")?.addValidators(Validators.min(5000))
+              this.form.get("price")?.addValidators(Validators.max(500000))
+
+              this.form.get("price")?.updateValueAndValidity();
+              break;
+            case "clothes":
+              this.form.get("price")?.clearValidators();
+
+              this.form.get("price")?.addValidators(Validators.max(50000))
+
+              this.form.get("price")?.updateValueAndValidity();
+              break;
+            case "food":
+              this.form.get("price")?.clearValidators();
+
+              this.form.get("price")?.addValidators(Validators.min(100))
+              this.form.get("price")?.addValidators(Validators.max(10000))
+
+              this.form.get("price")?.updateValueAndValidity();
+              break;
+          }
+        }
+      )
+
   }
 
 }
